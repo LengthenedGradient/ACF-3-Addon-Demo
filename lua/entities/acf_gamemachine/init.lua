103204071da528b94a72a8611f0ee053be317b6f
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2024-04-11 14:00:21 -0400

    Add Method Prototypes For Entities and Decide Wire Inputs

--- lua/entities/acf_gamemachine/init.lua
+++ lua/entities/acf_gamemachine/init.lua
@@ -2,3 +2,27 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include("shared.lua")
+
+local Inputs = {
+    "Dump (If set to a non-zero value, it will eject its contents)",
+}
+local Outputs = {
+    "Tickets (Tickets left in this box.)",
+    "Entity (The ticket box itself.) [ENTITY]",
+}
+
+local function VerifyData(Data) end
+
+local function UpdateGameMachine(Entity, Data, Class, Weapon, Ammo) end
+
+function MakeACF_Ammo(Player, Pos, Ang, Data) end
+
+function ENT:Update(Data) end
+
+function ENT:ACF_Activate(Recalc) end
+
+function ENT:ACF_OnDamage(DmgResult, DmgInfo) end
+
+function ENT:Detonate() end
+
+function ENT:UpdateOverlayText() end
